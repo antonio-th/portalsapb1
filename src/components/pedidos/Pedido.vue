cf8a3ac7be80e9b35de8925886231f3f8e2fbdac
Antonio Trujillo <antonio.trujillo01@gmail.com>
2018-02-15 01:21:35 -0600

    Ajuste del diseno en la pantalla de pedidos Creacion de la estructura de datos

--- src/components/pedidos/Pedido.vue
+++ src/components/pedidos/Pedido.vue
@@ -15,51 +15,51 @@
 
       <!-- Encabezado -->
       <div id="encabezado">
-        <el-row :gutter="15">
+        <el-row>
           <el-col :span="3">Docnum</el-col>
           <el-col :span="5">{{ pedido.docNum || '&nbsp;'}}&nbsp;</el-col>
           <el-col :span="4">Fecha Pedido</el-col>
           <el-col :span="11">{{ pedido.fechaPedido }}&nbsp;</el-col>
         </el-row>
 
-        <el-row :gutter="15">
+        <el-row>
           <el-col :span="3">Estatus</el-col>
           <el-col :span="5">{{ pedido.estatus || '&nbsp;' }}&nbsp;</el-col>
           <el-col :span="4">Fecha Entrega</el-col>
           <el-col :span="11">
-            <el-date-picker></el-date-picker>
+            <el-date-picker v-model="pedido.fechaEntrega"></el-date-picker>
           </el-col>
         </el-row>
 
-        <el-row :gutter="15">
+        <el-row>
           <el-col :span="3">Referencia</el-col>
           <el-col :span="5">
-            <el-input></el-input>
+            <el-input v-model="pedido.referencia"></el-input>
           </el-col>
           <el-col :span="4">Direccion Entrega</el-col>
           <el-col :span="11">
-            <el-date-picker></el-date-picker>
+            <el-select v-model="pedido.direccionEntrega.shipToCode"></el-select>
           </el-col>
         </el-row>
 
-        <el-row :gutter="15">
+        <el-row>
           <el-col :span="3">Moneda</el-col>
           <el-col :span="5">{{ pedido.moneda }}&nbsp;</el-col>
           <el-col :span="4">Cliente</el-col>
           <el-col :span="11">
-            <el-date-picker></el-date-picker>
+            <el-input v-model="pedido.cliente.cardCode"></el-input>
           </el-col>
         </el-row>
 
-        <el-row :gutter="15">
+        <el-row>
           <el-col :span="5" :offset="8">Nombre Comercial</el-col>
-          <el-col :span="8"></el-col>
+          <el-col :span="8">{{ pedido.cliente.cardFName }}</el-col>
         </el-row>
       </div> <!-- encabezado -->
 
       <!-- Partidas -->
       <div id="partidas">
-        <el-row>
+        <el-row id="th-partida">
           <el-col :span="4">Codigo</el-col>
           <el-col :span="6">Descripcion</el-col>
           <el-col :span="2">Cantidad</el-col>
@@ -70,21 +70,35 @@
           <el-col :span="2">Existencia</el-col>
           <el-col :span="2">&nbsp;</el-col>
         </el-row>
+        <el-row v-for="(partida, index) in pedido.partidas" :key="partida.id">
+          <el-col :span="4">{{ partida.itemCode}}</el-col>
+          <el-col :span="6">{{ partida.itemName}}</el-col>
+          <el-col :span="2">
+            <el-input v-model="partida.cantidad" size="mini"
+                      @input="recalcular(index)" />
+          </el-col>
+          <el-col :span="2">{{ partida.precio }}</el-col>
+          <el-col :span="2">{{ partida.impuestos }}</el-col>
+          <el-col :span="2">{{ partida.total }}</el-col>
+          <el-col :span="2">{{ partida.unidadMedida }}</el-col>
+          <el-col :span="2">{{ partida.existencia }}</el-col>
+          <el-col :span="2">&nbsp;</el-col>
+        </el-row>
       </div> <!-- partidas -->
 
       <!-- totales -->
       <div id="totales">
         <el-row>
           <el-col :span="2" :offset="14">Subtotal</el-col>
-          <el-col :span="2">0.0</el-col>
+          <el-col :span="2">{{ pedido.subTotal }}</el-col>
         </el-row>
         <el-row>
           <el-col :span="2" :offset="14">Impuestos</el-col>
-          <el-col :span="2">0.0</el-col>
+          <el-col :span="2">{{ pedido.impuestos }}</el-col>
         </el-row>
         <el-row>
           <el-col :span="2" :offset="14">Total</el-col>
-          <el-col :span="2">0.0</el-col>
+          <el-col :span="2">{{ pedido.total }}</el-col>
         </el-row>
       </div> <!-- totales -->
 
@@ -92,7 +106,8 @@
         <el-row>
           <el-col :span="4">Comentarios</el-col>
           <el-col :span="12">
-            <el-input type="textarea" :rows="4"></el-input>
+            <el-input type="textarea"
+                      :rows="4" v-model="pedido.comentarios" />
           </el-col>
         </el-row>
       </div>
@@ -106,8 +121,35 @@
   </div>
 </template>
 
-<script src="./Pedido.js" />
+<script>
+import * as PedidoService from './PedidoService'
+
+const data = () => {
+  return {
+    pedido: PedidoService.pedido
+  }
+}
+
+const metodos = {
+  cancelar () {
+    this.$router.push('/pedidos-list')
+  },
+  recalcular (index) {
+    console.log(index)
+  }
+}
+
+export default {
+  name: 'Pedido',
+  data: data,
+  methods: metodos
+}
+</script>
 <style>
+  #contenido {
+    margin: 0 10px 0 10px;
+  }
+
   #partidas, #encabezado, #comentarios, #toolbar {
     margin: 15px 0 0 0;
     border: solid 1px lightgrey;
@@ -120,5 +162,12 @@
 
   #encabezado {
     padding: 15px 0 15px 10px;
+  }
+
+  .borde {
+    border: solid 1px black;
+  }
+  #th-partida {
+    padding: 5px 0 5px 0;
   }
 </style>
--- src/components/pedidos/PedidoService.js
+++ src/components/pedidos/PedidoService.js
@@ -1,16 +1,39 @@
-export const fields = {
-  pedido: {
-    docNum: '',
-    fechaPedido: null,
-    fechaEntrega: null,
-    estatus: '',
-    referencia: '',
-    entrega: '',
-    moneda: '',
-    cliente: {},
-    partidas: [],
-    subTotal: 0,
-    impuestos: 0,
-    total: 0
-  }
+export let direccionEntrega = {
+  shipToCode: '',
+  direccion: ''
+}
+
+export let cliente = {
+  cardCode: '',
+  cardName: '',
+  cardFName: ''
+}
+
+export let partida = {
+  id: 0,
+  itemCode: 'HB30060',
+  itemName: 'jabon para manos',
+  cantidad: 2,
+  precio: 20,
+  impuestos: 0.2,
+  total: 0,
+  unidadMedida: 'PZA',
+  existencia: 20
+}
+
+export let pedido = {
+  id: 0,
+  docNum: '',
+  fechaPedido: null,
+  estatus: '',
+  fechaEntrega: null,
+  referencia: '',
+  direccionEntrega: direccionEntrega,
+  moneda: '',
+  cliente: cliente,
+  partidas: [partida],
+  comentarios: '',
+  subTotal: 0,
+  impuestos: 0,
+  total: 0
 }
